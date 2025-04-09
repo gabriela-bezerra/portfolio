43b10f8ec5a3b492e48e22000b9ff4d81776f936
Gabriela <gabrielaa.bdasilva@gmail.com>
2025-04-08 17:06:19 -0700

    Customize homepage with bio content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">About Me</h1>
+            <p className="text-lg">This is the About page!</p>
+        </div>
+    );
+}
